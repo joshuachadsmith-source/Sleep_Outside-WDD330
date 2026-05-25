209ec85352c330b792a2bf3569d17e8783784455
Josh Smith <joshuachadsmith@gmail.com>
2026-05-25 16:19:15 -0600

    commented out unused functions

--- src/js/product.js
+++ src/js/product.js
@@ -8,13 +8,13 @@ const dataSource = new ProductData('tents');
 const productDetails = new ProductDetails(productId, dataSource);
 productDetails.init();
 
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+// // add listener to Add to Cart button
+// document
+//   .getElementById('addToCart')
+//   .addEventListener('click', addToCartHandler);
